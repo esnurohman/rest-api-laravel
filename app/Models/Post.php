3f6604ec1e0f939eb8eacfdc9809ad78853aecb4
esnurohman <es.nurohman.5@gmail.com>
2024-09-15 21:24:14 +0700

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    // mass assignment dengan field yang sudah dibuat di migration
+    protected $fillable = [
+        'image',
+        'title',
+        'content',
+    ];
+
+    // accessor image
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image),
+        );
+    }
+}
